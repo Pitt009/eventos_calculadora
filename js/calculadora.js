2e8c17bd4f37aec85454c254cea6cf607a1edf1a
Pitt009 <gmartinez28837@gmail.com>
2017-03-22 15:12:40 -0600

    calculadora_2

--- js/calculadora.js
+++ js/calculadora.js
@@ -7,3 +7,13 @@ function mostrarNum(num){
    var resultado = document.getElementById("caja");
    resultado.value = eval(resultado.value);
    }
+
+function validarNumero(event){
+  var codigoASCII = event.keyCode;
+  if(codigoASCII == 13){
+    operacion();
+  }
+  if(codigoASCII < 42 || codigoASCII > 57) {
+    return false;
+}
+}
